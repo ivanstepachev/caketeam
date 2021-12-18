69ac28768a1ce6c63f7244323b9879c96544cd40
ivanstepacev <ivanstepatchev@gmail.com>
2021-12-19 02:10:58 +0300

    3

--- service/urls.py
+++ service/urls.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from django.urls import path
 from quiz import views as quiz_views
+from service import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', quiz_views.quiz, name="quiz"),
     path('orders', quiz_views.orders, name="orders"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
