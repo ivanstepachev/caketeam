47754396417251e61ae0722644a77232aa4da8f1
ivanstepacev <ivanstepatchev@gmail.com>
2021-12-19 02:15:10 +0300

    4

--- service/urls.py
+++ service/urls.py
@@ -8,7 +8,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', quiz_views.quiz, name="quiz"),
     path('orders', quiz_views.orders, name="orders"),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
