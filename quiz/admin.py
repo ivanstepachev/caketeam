e8597839382213cc0c5c75f7a8ab5acf10e46296
ivanstepacev <ivanstepatchev@gmail.com>
2022-01-13 01:56:13 +0300

    forms for webhooks

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from quiz.models import Order, Token, Staff
+from quiz.models import Order, Token, Staff, Note
 
 
 admin.site.register(Order)
+admin.site.register(Note)
 admin.site.register(Token)
 admin.site.register(Staff)
 
--- quiz/models.py
+++ quiz/models.py
@@ -6,6 +6,9 @@ class Note(models.Model):
     text = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return f'{self.date} {self.text}'
+
 
 class Order(models.Model):
     name = models.CharField(max_length=200, default='')
