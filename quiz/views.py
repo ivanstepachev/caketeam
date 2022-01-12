1101d6b4b7fe6202e6ddd3df1d65a396fd744de1
ivanstepacev <ivanstepatchev@gmail.com>
2022-01-13 02:55:12 +0300

    forms for webhooks

--- quiz/views.py
+++ quiz/views.py
@@ -43,7 +43,7 @@ def order_detail(request, order_id):
         comments = ''
         notes = Note.objects.filter(order=order)
         for note in notes:
-            comments += note.text
+            comments += '\n' + note.date + note.text
         order_text = f'''Десерт: {order.type_of_cake}
                 Примечание: {order.message}
                 Комментарии: {comments}'''
