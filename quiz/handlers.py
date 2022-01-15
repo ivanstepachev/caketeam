370323b5a29d14ccb0b648c258fb2f18ddf35f7b
ivanstepacev <ivanstepatchev@gmail.com>
2022-01-16 01:49:00 +0300

    add handlers for bot

--- quiz/handlers.py
+++ quiz/handlers.py
@@ -8,7 +8,7 @@ def send_message(chat_id, text, reply_markup=None, **kwargs):
     method = "sendMessage"
     token = Token.objects.get(id=1).token
     url = f"https://api.telegram.org/bot{token}/{method}"
-    data = {"chat_id": chat_id, "text": text}
+    data = {"chat_id": chat_id, "text": text, "reply_markup": reply_markup}
     requests.post(url, data=data)
 
 
@@ -21,3 +21,5 @@ def handler(chat_id, text):
         send_message(chat_id=chat_id, text=answer, reply_markup=reply_markup)
     else:
         send_message(chat_id=chat_id, text=text)
+
+        # json.dumps({"inline_keyboard": [[{"text": "yes", "callback_data": "yes"}]]})
