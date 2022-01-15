9c14aaad4d6d4a283e5beb7a262eadd228d467fd
ivanstepacev <ivanstepatchev@gmail.com>
2022-01-16 01:45:04 +0300

    add handlers for bot

--- quiz/handlers.py
+++ quiz/handlers.py
@@ -15,7 +15,9 @@ def send_message(chat_id, text, reply_markup=None, **kwargs):
 def handler(chat_id, text):
     if text == '/start':
         answer = '''Добро пожаловать на платформу. Здесь ты можешь получать кондитерские задание. Нажми начать для старта'''
-        reply_markup = json.dumps({"inline_keyboard": [[{"text": "yes", "callback_data": "yes"}]]})
+        reply_markup = json.dumps({'keyboard': [["A button"], ["B button"]],
+                            'one_time_keyboard': False,
+                            'resize_keyboard': True})
         send_message(chat_id=chat_id, text=answer, reply_markup=reply_markup)
     else:
         send_message(chat_id=chat_id, text=text)
