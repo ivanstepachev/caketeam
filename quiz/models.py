3c02fb64f5a049da3b73a5f41c0b8bacb840ad1c
ivanstepacev <ivanstepatchev@gmail.com>
2022-01-27 00:30:14 +0300

    add handlers for bot

--- quiz/models.py
+++ quiz/models.py
@@ -78,13 +78,12 @@ class Image(models.Model):
         return str(self.image)
 
     def save(self, *args, **kwargs):
-        super(Image, self).save(*args, **kwargs)
-        img = PillowImage.open(self.image.path)
-        if img.height > 300 or img.width > 300:
+        if self.image:
+            super().save(*args, **kwargs)
+            img = PillowImage.open(self.image.path)
             output_size = (300, 300)
             img.thumbnail(output_size)
             img.save(self.image.path)
-
 
 class Token(models.Model):
     token = models.CharField(max_length=30)
