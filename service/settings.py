51fe56c0ec47f66898b4292074c4b88fc7e01f10
ivanstepacev <ivanstepatchev@gmail.com>
2022-02-19 01:12:04 +0300

    add postgresql

--- service/settings.py
+++ service/settings.py
@@ -146,8 +146,8 @@ STATICFILES_DIRS = (
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = 'media/'
-MEDIA_ROOT = 'media'
-# MEDIA_ROOT = os.environ.get('EDGE_URL', '') + '/media/'
+
+MEDIA_ROOT = os.environ.get('EDGE_URL', '') + '/media/'
 
 LOGOUT_REDIRECT_URL = '/'
 LOGIN_REDIRECT_URL = '/a/orders'
